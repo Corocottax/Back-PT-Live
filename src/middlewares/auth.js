232f2cc0c6e8ea5d551bc93c649471eeffdfaa8b
Corocottax <santicorocottax@gmail.com>
2023-03-07 20:11:58 +0100

    erorr

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,7 +10,7 @@ const isAuth = async (req, res, next) => {
 
         //*  si no tiene ese campo entonces significa que no está logueado, pues no le dejo pasar
         if (!token) {
-            return res.json("No estás autorizado crack");
+            return res.status(500).json("No estás autorizado crack");
         }
 
         //* de tener token tenemos que comprobar que sea válido, para ello, necesitamos sólo el token, pero la autorización me escribe Bearer dkfjlasdfj, por lo tanto, quitamos el Bearer*espacio*
